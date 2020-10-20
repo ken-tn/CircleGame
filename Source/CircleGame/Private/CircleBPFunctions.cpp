c3bc656e88d07756759d7a7763c49dfed8a4e58c
kent911t <38088995+kent911t@users.noreply.github.com>
2020-10-20 19:10:48 +0100

    updated stuff

--- Source/CircleGame/Private/CircleBPFunctions.cpp
+++ Source/CircleGame/Private/CircleBPFunctions.cpp
@@ -28,7 +28,7 @@ void UCircleBPFunctions::RaycastFromCamera(const AActor* Camera, const TArray<AA
 	//ConvertMouseLocationToWorldSpace doesn't exist in C++, scrapped :)
 }
 
-void UCircleBPFunctions::OrbitLoop(const AActor* center, AActor* object, const float radius, const float eclipse, const float orbitTime, const float currentRotation, bool reverse, float& newRotation)
+void UCircleBPFunctions::OrbitLoop(const AActor* center, AActor* object, const float radius, const float eclipse, const float orbitTime, const float currentRotation, bool reverse, float& newRotation, FVector& newPosition)
 {
 	float rotationSpeed = PI * 2 / orbitTime;
 	float calculateEclipse = eclipse * radius;
@@ -40,7 +40,59 @@ void UCircleBPFunctions::OrbitLoop(const AActor* center, AActor* object, const f
 	}
 
 	FVector mainOrbiterPosition = center->GetActorLocation();
-	FVector newPosition = FVector(sin(newRotation) * calculateEclipse, cos(newRotation) * radius, 0) + mainOrbiterPosition;
-
-	object->SetActorLocation(newPosition);
+	newPosition = FVector(sin(newRotation) * calculateEclipse, cos(newRotation) * radius, 0) + mainOrbiterPosition;
 }
+
+void UCircleBPFunctions::CloneOrbiter(const AActor* mainOrbiter, UStaticMeshComponent*& staticMeshComponent, AActor*& actor)
+{
+	FTransform spawnTransform = mainOrbiter->GetRootComponent()->GetComponentTransform();
+	actor = mainOrbiter->GetWorld()->SpawnActor<AActor>(spawnTransform.GetLocation(), spawnTransform.GetRotation().Rotator());
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>MeshAsset(TEXT("StaticMesh'/Game/Circles/orbit.orbit'"));
+	UStaticMesh* orbiterMesh = MeshAsset.Object;
+	staticMeshComponent = actor->CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	staticMeshComponent->SetStaticMesh(orbiterMesh);
+
+	static ConstructorHelpers::FObjectFinder<UMaterialInstance>MaterialAsset(TEXT("MaterialInstanceConstant'/Game/Material/Orbit_Material.Orbit_Material'"));
+	UMaterialInstance* orbiterMaterial = MaterialAsset.Object;
+	staticMeshComponent->SetMaterial(0, orbiterMaterial);
+}
+
+void UCircleBPFunctions::RingInRange(const AActor* orbiter, const AActor* currentTarget, const float lockRadius, float &distance, bool& inRange)
+{
+	FVector orbiterLocation = orbiter->GetActorLocation();
+	FVector ringLocation = currentTarget->GetActorLocation();
+	distance = (orbiterLocation - ringLocation).Size();
+
+	if (distance < lockRadius) {
+		inRange = true;
+	}
+	else {
+		inRange = false;
+	}
+
+	return;
+}
+
+void UCircleBPFunctions::FindNearestRing(const TMap<AActor*, float> nearRings, AActor*& nearestRing)
+{
+	float minDist = 100000.f;
+	for (auto It = nearRings.CreateConstIterator(); It; ++It)
+	{
+		const float dist = It.Value();
+		if (dist < minDist) {
+			minDist = dist;
+			nearestRing = It.Key();
+		}
+	}
+}
+
+UCircleBPFunctions::UCircleBPFunctions(const FObjectInitializer& ObjectInitializer) :
+	Super(ObjectInitializer)
+	{
+		static ConstructorHelpers::FObjectFinder<UStaticMesh>MeshAsset(TEXT("StaticMesh'/Game/Circles/orbit.orbit'"));
+		static ConstructorHelpers::FObjectFinder<UMaterialInstance>MaterialAsset(TEXT("MaterialInstanceConstant'/Game/Material/Orbit_Material.Orbit_Material'"));
+
+		orbiterMesh = MeshAsset.Object;
+		orbiterMaterial = MaterialAsset.Object;
+	}
--- Source/CircleGame/Public/CircleBPFunctions.h
+++ Source/CircleGame/Public/CircleBPFunctions.h
@@ -13,7 +13,16 @@
 UCLASS()
 class CIRCLEGAME_API UCircleBPFunctions : public UBlueprintFunctionLibrary
 {
+	
 	GENERATED_BODY()
+		UCircleBPFunctions(const FObjectInitializer& ObjectInitializer);
+
+
+		UPROPERTY()
+			UStaticMesh* orbiterMesh;
+
+		UPROPERTY()
+			UMaterialInstance* orbiterMaterial;
 
 		UFUNCTION(BlueprintCallable, Category = "CircleFunctions")
 			static const FRotator NormalToRotator(FVector currentRotation, FVector normal);
@@ -22,5 +31,14 @@ class CIRCLEGAME_API UCircleBPFunctions : public UBlueprintFunctionLibrary
 			static void RaycastFromCamera(const AActor* Camera, const TArray<AActor*> ignore, FHitResult &outHit, bool &hit);
 
 		UFUNCTION(BlueprintCallable, Category = "CircleOrbiter")
-			static void OrbitLoop(const AActor* center, AActor* object, const float radius, const float eclipse, const float orbitTime, const float currentRotation, bool reverse, float &newRotation);
+			static void OrbitLoop(const AActor* center, AActor* object, const float radius, const float eclipse, const float orbitTime, const float currentRotation, bool reverse, float &newRotation, FVector& newPosition);
+
+		UFUNCTION(BlueprintCallable, Category = "CircleRings")
+			static void CloneOrbiter(const AActor* mainOrbiter, UStaticMeshComponent*& staticMeshComponent, AActor*& actor);
+		
+		UFUNCTION(BlueprintCallable, Category = "CircleRings")
+			static void RingInRange(const AActor* orbiter, const AActor* currentTarget, const float lockRadius, float& distance, bool& inRange);
+
+		UFUNCTION(BlueprintCallable, Category = "CircleRings")
+			static void FindNearestRing(const TMap<AActor*, float> nearRings, AActor*& nearestRing);
 };
